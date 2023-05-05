9f8f63a052132512a579912e7825b5a519bde3ec
silverdragon25 <62597397+silverdragon25@users.noreply.github.com>
2023-05-05 14:14:47 +0200

    aaaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ ProjectAR/Assets/Imagine/ImageTracker/Demos/Scripts/DemoFPSCounter.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// This script calculate the current fps and show it to a text ui.
+/// </summary>
+
+namespace Imagine.WebAR.Samples
+{
+    public class DemoFPSCounter : MonoBehaviour
+    {
+        [SerializeField] private Text fpsText;
+
+        public float updateRateSeconds = 4.0F;
+
+        int frameCount = 0;
+        float dt = 0f;
+        float fps = 0f;
+
+        void Update()
+        {
+            frameCount++;
+            dt += Time.unscaledDeltaTime;
+            if (dt > 1f / updateRateSeconds)
+            {
+                fps = frameCount / dt;
+                frameCount = 0;
+                dt -= 1f / updateRateSeconds;
+            }
+            fpsText.text = System.Math.Round(fps, 1).ToString("0.0");
+        }
+    }
+}
